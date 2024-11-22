ac961b0df81aa5db271fee9bdcc1c375f1a1e047
shweta11soni <io1912shweta@gmail.com>
2024-11-22 18:27:27 +0530

    Initial Commit

---
+++ SpyneIOSMainAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpyneIOSMainAssessment
+//
+//  Created by shweta soni on 22/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
